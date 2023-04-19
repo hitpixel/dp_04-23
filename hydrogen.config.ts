c3c134d2874bea83b8bac75c3d22867173885478
hitpixel <59998304+hitpixel@users.noreply.github.com>
2023-04-19 16:08:12 +1000

    Update hydrogen.config.ts

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -9,11 +9,11 @@ export default defineConfig({
       Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
     storeDomain:
       // @ts-ignore
-      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'oxygenator.myshopify.com',
+      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'dreamplants-5817.myshopify.com',
     storefrontToken:
       // @ts-ignore
       Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
-      '70faab4b482211c4167f94181a4ba4ed',
+      '6ca6b13ea48bc57148db8a7e971f5e1ed',
     storefrontApiVersion: '2022-07',
     // @ts-ignore
     storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID,
